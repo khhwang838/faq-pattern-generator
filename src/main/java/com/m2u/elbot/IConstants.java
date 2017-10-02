a4dc2e772675dc80961d5f1bd4a989825660fd89
Kihyun Hwang <kihyun.hwang@m2utech.com>
2017-10-02 16:42:14 +0900

    로직 일부 수정

--- src/main/java/com/m2u/elbot/IConstants.java
+++ src/main/java/com/m2u/elbot/IConstants.java
@@ -5,6 +5,8 @@ public interface IConstants {
 	String question = "질문";
 	String keyword = "키워드";
 	String response = "답변";
+	String faqSheetName = "FAQ";
+	
 	interface Defaults {
 		String category = "자동생성";
 		String separator = ";";
