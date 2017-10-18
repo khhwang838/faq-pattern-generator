c66a1113083ba1a498cc850d34a8d8a0a7a6afd0
Kihyun Hwang <kihyun.hwang@m2utech.com>
2017-10-19 08:29:32 +0900

    카테고리 구분하여 등록할 수 있도록 수정

--- src/main/java/com/m2u/elbot/IConstants.java
+++ src/main/java/com/m2u/elbot/IConstants.java
@@ -2,13 +2,17 @@ package com.m2u.elbot;
 
 public interface IConstants {
 
+	String catetory1 = "대분류";
+	String catetory2 = "중분류";
+	String catetory3 = "소분류";
+
 	String question = "질문";
 	String keyword = "키워드";
 	String response = "답변";
 	String faqSheetName = "FAQ";
 	
 	interface Defaults {
-		String category = "자동생성";
+		String category = "기타";
 		String separator = ";";
 	}
 	
